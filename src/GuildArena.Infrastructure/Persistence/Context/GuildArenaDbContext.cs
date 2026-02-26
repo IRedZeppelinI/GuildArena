b8fddfae541226284d970354f9089794981613e5
Pedro Verde <pedroverde@gmail.com>
2026-02-26 18:42:25 +0000

    Add PersistenceContext class. Add Design package to API, add postgres to docker-compose and uupdate appsettings to address sql container

---
+++ src/GuildArena.Infrastructure/Persistence/Context/GuildArenaDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GuildArena.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GuildArena.Infrastructure.Persistence.Context;
+
+public class GuildArenaDbContext : IdentityDbContext<ApplicationUser>
+{
+    public GuildArenaDbContext(DbContextOptions<GuildArenaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Guild> Guilds => Set<Guild>();
+    public DbSet<Hero> Heroes => Set<Hero>();
+    public DbSet<Match> Matches => Set<Match>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {        
+        base.OnModelCreating(builder);
+
+        // Aplica as  configurações 
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
